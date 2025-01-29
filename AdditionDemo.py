9b2e0445457aff173d162f5aab9de64ebcd3864b
Conner Holcomb <conner.holcomb.16@gmail.com>
2025-01-29 13:11:48 -0500

    Update AdditionDemo.py

---
+++ AdditionDemo.py
@@ -0,0 +1,11 @@
+
+number1 = 0
+number2 = 0
+
+print("please enter two numbers")
+number1 = int(input("number 1: "))
+number2 = int(input("number 2: "))
+
+sum = number1 + number2
+
+print(f"The sum of {number1} and {number2} is {sum}.")
