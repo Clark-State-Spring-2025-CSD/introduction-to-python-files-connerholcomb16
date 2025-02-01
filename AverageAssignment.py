61fd0127547bcdcdc99b600de9cc200b84ab4da0
Conner Holcomb <conner.holcomb.16@gmail.com>
2025-01-31 23:35:07 -0500

    Update AverageAssignment.py

---
+++ AverageAssignment.py
@@ -0,0 +1,15 @@
+clone1 = 0
+clone2 = 0
+clone3 = 0
+
+print("enter 3 numbers")
+clone1 = int(input("clone 1: "))
+clone2 = int(input("clone 2: "))
+clone3 = int(input("clone 3: "))
+
+sum = clone1 + clone2 + clone3 
+
+average = ((clone1 + clone2 + clone3)/3)
+print(f"the sum of {clone1} and {clone2} and {clone3} is {sum}")
+print(f"the average is {average}")
+
